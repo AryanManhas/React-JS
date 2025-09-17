8d2a1034a938db18ae91fdd603fcaed512a88422
Aryan Manhas <2022A1R033@mietjammu.in>
2025-09-17 17:49:04 +0530

    add react basics

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Aryan and React!! | Aryan</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>Aryan And Vite | Aryan</h1>
+  )
+}
+
+export default App
