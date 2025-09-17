996bc07bf7455cb133d7ddddccb1cc76888c23a4
Aryan Manhas <2022A1R033@mietjammu.in>
2025-09-17 19:19:39 +0530

    react app

---
+++ todos-list/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import Header from "./MyComponents/Header";
+import Footer from "./MyComponents/Footer";
+import TodoItem   from "./MyComponents/TodoItem";
+import Todos  from "./MyComponents/Todos";
+
+function App() {
+  return (
+    <>
+    <Header title="MyTodosList" />
+    <Todos/>
+    <Footer/>
+    <TodoItem/>
+    </>
+  );
+}
+
+export default App;
